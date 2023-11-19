528a9faa1abd6e252b3ab55eb1f9d9975acb52be
amogus3000 <bvalentin1415@gmail.com>
2023-11-19 23:06:22 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ban aggressor"
+include(":app")
+include(":data")
+include(":features")
+include(":core")
+include(":core:common")
